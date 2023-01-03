6746c8819920f08d7d80aee852aac89fd25f226f
PaulCalderon <calderon.paul.august@gmail.com>
2023-01-03 19:01:25 +0800

    complete update functionality

--- contact_book_program.py
+++ contact_book_program.py
@@ -2,6 +2,8 @@ from contact import Contact
 from contact import Contact_Record
 from contact_book_repository import create_contact
 from contact_book_repository import delete_contact
+from contact_book_repository import update_contact
+from contact_book_repository import close
 #from contact_book_repository import *
 
 
@@ -9,13 +11,13 @@ def action_chosen_from_input(chosen_action):
     if chosen_action == 1:
         create()
     elif chosen_action == 2:
-        contact_book_update_contact()
+        update()
     elif chosen_action == 3:
         delete()
     elif chosen_action == 4:
         contact_book_list_contact()
     elif chosen_action == 5:
-        contact_book_close()  
+        close() 
         exit()
     else:
         print("\nAction chosen was invalid\n")
@@ -52,6 +54,25 @@ def create():
 def delete():
     print("Enter the contact ID you wish to delete")
     delete_contact(int(input()))
+    print("*************\nDeleting contact was successful\n*************")
+
+
+def update():
+    print("Enter the contact ID you wish to update")
+    cid = str(input())
+    print("Enter the updated name (Leave blank if you wish to retain)")
+    contact_name = input()
+    print("Enter the updated city (Leave blank if you wish to retain)")
+    contact_city = input()
+    print("Enter the updated contact number (Leave blank if you wish to retain)")
+    contact_number = input()
+    print("Enter the updated email address (Leave blank if you wish to retain)")
+    contact_email = input()
+    contact_to_update = Contact(cid, contact_name, contact_city, 
+        contact_number, contact_email)
+    update_contact(contact_to_update)
+    print("*************\nUpdating contact was successful\n*************")
+
 
 if __name__ == "__main__":
     contact_book_home_screen()
